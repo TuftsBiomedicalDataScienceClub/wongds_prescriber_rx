d3ac279ec73a95b39ad1f1b923391cf2d77d4f41
Daniel Wong <dsw2107@gmail.com>
2017-02-27 16:27:25 -0500

    Selected opioid rx data from prescriber attr dataset
    Prescriber attribute dataset too large to simply expand. Instead, walk
    trhough dataset searching for opioids in list of prescribed drugs, and
    build a list of prescribers who have prescribed opioids, keeping track
    of which drug and the prescriber's NPI number. Use this dataframe to
    unify prescriber attributes and opioid prescription.

--- prescriber_rx.py
+++ prescriber_rx.py
@@ -1,6 +1,15 @@
+'''
+# wongds_prescriber_rx
+A Python-based approach for a Tufts Biomedical Data Science Club project to determine whether patterns exist between prescriber demographic information and opioid prescription.
+
+## Datasets
+- Prescriber attributes: https://www.kaggle.com/roamresearch/prescriptionbasedprediction
+- Opiate prescriptions: https://www.kaggle.com/apryor6/us-opiate-prescriptions
+'''
 # prescriber_rx.py
 # Daniel Wong 26 Feb 2017
 
+# Load some useful libraries
 import csv
 import jsonlines
 import copy
@@ -8,47 +17,45 @@ from pprint import pprint
 import pandas as pd
 import numpy as np
 
-# Process the prescriber information dataset first, to convert from JSON to dataframe
-## Location of dataset
-prescriber_dataset = 'dataset_prescriber/roam_prescription_based_prediction.jsonl'
+# Load data
+# Opiate dataset
+opiate_class_data = 'dataset_rx/opioids.csv'
+opiate_prescriber_data = 'dataset_rx/prescriber-info.csv'
+opiate_overdose_data = 'dataset_rx/overdoses.csv'
 
-## Build a list of dictionaries containing prescriber data
-prescriber_data = []
-with jsonlines.open(prescriber_dataset) as reader:
+# List of opiates
+opioids = pd.read_csv(opiate_class_data)
+
+# Opiate prescriber info
+opiate_prescribers = pd.read_csv(opiate_prescriber_data)
+
+# Opiate overdoses, by US State
+opiate_overdoses = pd.read_csv(opiate_overdose_data)
+
+# Prescriber attribute dataset
+prescriber_attr_dataset = 'dataset_prescriber/roam_prescription_based_prediction.jsonl'
+
+# Build a list of dictionaries containing prescriber data
+prescriber_attr_data = []
+with jsonlines.open(prescriber_attr_dataset) as reader:
     for obj in reader:
-        prescriber_data.append(obj)
-        
-## Set up column names for Pandas DataFrame
-column_names = ['drug_name', 'count', 'npi']
-column_names.extend(list(prescriber_data[0]['provider_variables'].keys()))
+        prescriber_attr_data.append(obj)
 
-## Set up the dataframe
-prescriptions = pd.DataFrame(np.nan, index=[], columns=column_names)
+# Look for prescription of opiates in prescriber_data
+opioid_prescribers = {'Drug Name': [],
+                      'npi': [],
+                     }
 
-## Migrate data into the dataframe
-### Re-format JSONL data to Pandas dataframe for CSV, one line for each prescriber/drug prescrbed combination
-for entry in prescriber_data:
-    # Prescriber information
-    prescriber_info = {'npi': entry['npi']}
-    prescriber_info.update(entry['provider_variables'])
-    
-    for drug_name, count in entry['cms_prescription_counts'].items():
-        data_row = {column_names[0]: drug_name,
-                    column_names[1]: count,
-                    column_names[2]: prescriber_info['npi'],
-                    column_names[3]: prescriber_info['settlement_type'],
-                    column_names[4]: prescriber_info['region'],
-                    column_names[5]: prescriber_info['specialty'],
-                    column_names[6]: prescriber_info['gender'],
-                    column_names[7]: prescriber_info['generic_rx_count'],
-                    column_names[8]: prescriber_info['brand_name_rx_count'],
-                    column_names[9]: prescriber_info['years_practicing'],
-                   }
-        #print(data_row, column_names)
-        dataframe_row = pd.DataFrame(data_row,
-                                     index=[1],
-                                     columns=column_names)
-        #print(dataframe_row)
-        prescriptions = pd.concat([prescriptions, dataframe_row], ignore_index=True)
-prescriptions.to_csv('prescriptions.csv')        
-#print(prescriptions)
+# Cycle through each prescriber record
+for prescriber in prescriber_attr_data:
+    # Look for opioids by Drug Name, and make a record of prescriber by npi.
+    # Generic names also appear in 'Drug Name' list.
+    for drug in opioids['Drug Name']:
+        if drug in prescriber['cms_prescription_counts'].keys():
+            opioid_prescribers['Drug Name'].append(drug)
+            opioid_prescribers['npi'].append(prescriber['npi'])
+
+# Convert the dictionary to a dataframe and save to csv.
+# Use this dataframe to unify prescriber attributes and opioid prescription.
+opioid_prescriber_df = pd.DataFrame(opioid_prescribers)
+opioid_prescriber_df.to_csv('opioid_prescribers.csv')
